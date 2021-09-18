89d42500a81f2d0acf408f22150c9c0ee1124b8b
Matheus Silva <54555111+matheuusu@users.noreply.github.com>
2021-09-18 16:37:46 -0300

    18/09/21
    routes settings

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import SignIn from './Pages/Signin';
+import RecoveryPass from './Pages/RecoveryPass';
+import Room from './Pages/Room';
 
 
 export default () => {    
@@ -8,6 +10,12 @@ export default () => {
         <Switch>
             <Route exact path="/">
                 <SignIn />
+            </Route>,
+            <Route exact path="/recovery-pass">
+                <RecoveryPass />
+            </Route>,
+            <Route exact path="/room">
+                <Room />
             </Route>
             
         </Switch>
